2a521a4be0d7ff864906142d6d3653ecefad4eae
JAFang <kjw472800@gmail.com>
2020-11-13 23:20:05 +0800

    tryv1

--- util/address2coords.js
+++ util/address2coords.js
@@ -4,7 +4,7 @@ const HttpError = require('../models/http-error');
 
 async function address2coords(address){
     
-    const response= await Axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${process.env.GOOGLE_API_KEY}`);
+    /*const response= await Axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${process.env.GOOGLE_API_KEY}`);
     const data=response.data;
 
     if(!data||data.status==='ZERO_RESULTS'){
@@ -13,8 +13,8 @@ async function address2coords(address){
     }
 
     const coordinates= data.results[0].geometry.location;
-    return coordinates;
-    // return { lat: 25.033976, lng: 121.5645389 };
+    return coordinates;*/
+    return { lat: 25.033976, lng: 121.5645389 };
 };
 
 module.exports=address2coords;
