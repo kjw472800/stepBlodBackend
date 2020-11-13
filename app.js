f1a5671503d318a8974a0fb213e89dd442b057fc
JAFang <kjw472800@gmail.com>
2020-11-14 00:24:28 +0800

    Fix json bodyparser

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ const HttpError = require('./models/http-error')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads','images')));
 // app.use((req,res,next)=>{
